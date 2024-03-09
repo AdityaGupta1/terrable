0d5bc623d54656c301a23c26ca747b62df074643
Aditya Gupta <adityag@gmail.com>
2024-03-09 15:11:27 +0100

    drastically simplify node cpp/h and make it work with geometry (need to make it work with heightfields instead)

--- src/terrable_plugin.cpp
+++ src/terrable_plugin.cpp
@@ -1,5 +1,4 @@
 #include <UT/UT_DSOVersion.h>
-//#include <RE/RE_EGLServer.h>
 
 #include <UT/UT_Math.h>
 #include <UT/UT_Interrupt.h>
@@ -10,28 +9,24 @@
 #include <PRM/PRM_SpareData.h>
 #include <OP/OP_Operator.h>
 #include <OP/OP_OperatorTable.h>
+#include <OP/OP_AutoLockInputs.h>
 
 #include <limits.h>
 #include "terrable_plugin.h"
 using namespace HDK_Sample;
 
-//
-// newSopOperator is the hook that Houdini grabs from this dll
-// and invokes to register the SOP. In this case we add ourselves
-// to the specified operator table.
-//
 void newSopOperator(OP_OperatorTable* table)
 {
     table->addOperator(
         new OP_Operator(
-            "TerrableMain", // Internal name
-            "Terrable", // UI name
-            SOP_Terrable::myConstructor, // How to build the SOP
-            SOP_Terrable::myTemplateList, // My parameters
-            0, // Min # of sources
-            0, // Max # of sources
-            SOP_Terrable::myVariables, // Local variables
-            OP_FLAG_GENERATOR // Flag it as generator
+            "TerrableMain", // internal name (also used for name of newly created node)
+            "Terrable", // UI name (shows up in node search)
+            SOP_Terrable::myConstructor, // constructor
+            SOP_Terrable::myTemplateList, // parameters
+            1, // min # of sources
+            1, // max # of sources
+            nullptr, // local variables
+            OP_FLAG_GENERATOR // flag it as generator
         )
     );
 }
@@ -45,38 +40,6 @@ PRM_Template SOP_Terrable::myTemplateList[] = {
     PRM_Template()
 };
 
-// Here's how we define local variables for the SOP.
-enum {
-    VAR_PT, // Point number of the star
-};
-
-CH_LocalVariable
-SOP_Terrable::myVariables[] = {
-    { "PT", VAR_PT, 0 }, // The table provides a mapping
-    { 0, 0, 0 }
-};
-
-bool SOP_Terrable::evalVariableValue(fpreal &val, int index, int thread)
-{
-    // myCurrPoint will be negative when we're not cooking so only try to
-    // handle the local variables when we have a valid myCurrPoint index.
-    if (myCurrPoint >= 0)
-    {
-        // Note that "gdp" may be null here, so we do the safe thing
-        // and cache values we are interested in.
-        switch (index)
-        {
-        case VAR_PT:
-            val = (fpreal) myCurrPoint;
-            return true;
-        default:
-            /* do nothing */;
-        }
-    }
-    // Not one of our variables, must delegate to the base class.
-    return SOP_Node::evalVariableValue(val, index, thread);
-}
-
 OP_Node* SOP_Terrable::myConstructor(OP_Network *net, const char *name, OP_Operator *op)
 {
     return new SOP_Terrable(net, name, op);
@@ -84,9 +47,7 @@ OP_Node* SOP_Terrable::myConstructor(OP_Network *net, const char *name, OP_Opera
 
 SOP_Terrable::SOP_Terrable(OP_Network *net, const char *name, OP_Operator *op)
     : SOP_Node(net, name, op)
-{
-    myCurrPoint = -1; // To prevent garbage values from being returned
-}
+{}
 
 SOP_Terrable::~SOP_Terrable() {}
 
@@ -97,47 +58,30 @@ unsigned SOP_Terrable::disableParms()
 
 OP_ERROR SOP_Terrable::cookMySop(OP_Context &context)
 {
-    fpreal now = context.getTime();
-
-    int simTimeYears = SIM_TIME(now);
-
-    UT_Interrupt* boss;
-
-    // Since we don't have inputs, we don't need to lock them.
-    myCurrPoint = 0; // Initialize the PT local variable
-
-    // Check to see that there hasn't been a critical error in cooking the SOP.
-    if (error() < UT_ERROR_ABORT)
+    OP_AutoLockInputs inputs(this);
+    if (inputs.lock(context) >= UT_ERROR_ABORT)
     {
-        boss = UTgetInterrupt();
-        gdp->clearAndDestroy();
-
-        if (boss->opStart("Building LSYSTEM"))
-        {
-            // TODO: terrain stuff goes here
-
-            vec3 start = { 0, 0, 0 };
-            vec3 end = { 0, (double)simTimeYears, 0 };
-
-            GU_PrimPoly* poly = GU_PrimPoly::build(gdp, 2);
-
-            GA_Offset startPtoff = poly->getPointOffset(0);
-            GA_Offset endPtoff = poly->getPointOffset(1);
-
-            gdp->setPos3(startPtoff, UT_Vector3(start[0], start[1], start[2]));
-            gdp->setPos3(endPtoff, UT_Vector3(end[0], end[1], end[2]));
-
-            ////////////////////////////////////////////////////////////////////////////////////////////
-
-            // Highlight the star which we have just generated. This routine
-            // call clears any currently highlighted geometry, and then it
-            // highlights every primitive for this SOP. 
-            select(GU_SPrimitive);
-        }
-
-        boss->opEnd();
+        return error();
     }
 
-    myCurrPoint = -1;
+    fpreal now = context.getTime();
+
+    int simTimeYears = getSimTime(now);
+
+    duplicateSource(0, context); // duplicate input geometry
+
+    fpreal frame = simTimeYears * 0.03;
+
+    GA_RWHandleV3 Phandle(gdp->findAttribute(GA_ATTRIB_POINT, "P"));
+    GA_Offset ptoff;
+    GA_FOR_ALL_PTOFF(gdp, ptoff)
+    {
+        UT_Vector3 Pvalue = Phandle.get(ptoff);
+        Pvalue.y() = sin(Pvalue.x() * .2 + Pvalue.z() * .3 + frame);
+        Phandle.set(ptoff, Pvalue);
+    }
+
+    Phandle.bumpDataId();
+
     return error();
 }
--- src/terrable_plugin.h
+++ src/terrable_plugin.h
@@ -1,7 +1,6 @@
 #ifndef __LSYSTEM_PLUGIN_h__
 #define __LSYSTEM_PLUGIN_h__
 
-//#include <GEO/GEO_Point.h>
 #include <SOP/SOP_Node.h>
 #include "LSystem.h"
 
@@ -13,39 +12,18 @@ class SOP_Terrable : public SOP_Node
 public:
     static OP_Node* myConstructor(OP_Network*, const char*, OP_Operator*);
 
-    // Stores the description of the interface of the SOP in Houdini.
-    // Each parm template refers to a parameter.
     static PRM_Template myTemplateList[];
-
-    // This optional data stores the list of local variables.
-    static CH_LocalVariable myVariables[];
 
 protected:
     SOP_Terrable(OP_Network *net, const char *name, OP_Operator *op);
     virtual ~SOP_Terrable();
 
-    // Disable parameters according to other parameters.
-    virtual unsigned disableParms();
+    unsigned disableParms() override;
 
-    // cookMySop does the actual work of the SOP computing
-    virtual OP_ERROR cookMySop(OP_Context &context);
-
-    virtual bool evalVariableValue(fpreal &val, int index, int thread);
-    virtual bool evalVariableValue(UT_String &v, int i, int thread)
-    {
-        return evalVariableValue(v, i, thread);
-    }
+    OP_ERROR cookMySop(OP_Context &context) override;
 
 private:
-    int SIM_TIME(fpreal t) { return evalInt("sim_time", 0, t); }
-
-    // Member variables are stored in the actual SOP, not with the geometry
-    // In this case these are just used to transfer data to the local 
-    // variable callback.
-    // Another use for local data is a cache to store expensive calculations.
-
-    // NOTE: You can declare local variables here like this  
-    int myCurrPoint;
+    int getSimTime(fpreal t) { return evalInt("sim_time", 0, t); }
 };
 
 } // End HDK_Sample namespace
